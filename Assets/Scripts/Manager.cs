3f2002834745e7bfa530c1760e634fb40496a185
unknown <ilonnaa12@gmail.com>
2021-03-17 18:12:02 +0200

    complition edition Maneger scripts. Implement Functionlity to controll UI comoonrnts on game over start

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -5,15 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
+
+    [SerializeField] GameObject[] gameOverComponents;
+    [SerializeField] GameObject[] startGameComponents;
+    [SerializeField] GameObject scoreObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1; // возобновления игри после паузы
+        DisableUIComponents(gameOverComponents);
+        EnableUIComponents(startGameComponents);
+        scoreObject.SetActive(false);
+
     }
 
-    public void RestartGame()
+    public void EnableScore()
     {
-        SceneManager.LoadScene(0);
+        scoreObject.SetActive(true);
     }
 
+    public void DisableStartUI()
+    {
+        DisableUIComponents(startGameComponents);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        EnableUIComponents(gameOverComponents);
+        DisableUIComponents(startGameComponents);
+    }
+    private void DisableUIComponents(GameObject[] objectsToDisable)
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private void EnableUIComponents(GameObject[] objectsToEnable)
+    {
+        foreach (GameObject obj in objectsToEnable)
+        {
+            obj.SetActive(true);
+        }
+    }
 }
